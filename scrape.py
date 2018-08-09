aeb728c632db5447b540e0a629f0c27d3b0d4420
ktech99 <kartikarora99@gmail.com>
2018-08-09 17:34:51 +0530

    print summaries for all reddits

--- scrape.py
+++ scrape.py
@@ -15,14 +15,12 @@ keywords = []
 # print(json.dumps(r.json()['data']['children'][0]))
 
 for post in r.json()['data']['children']:
-    if (not post['data']['author'] == "null"):
-        # print(post['data']['url'].encode("utf-8"))
+    if (not (post['data']['author'] == "null" or post['data']['url'] == "null")):
+        print(post['data']['url'].encode("utf-8"))
         # print(post['data']['permalink'].encode("utf-8"))
         # print(post['data']['author'].encode("utf-8"))
         # print(post['data']['thumbnail'].encode("utf-8"))
         # print(post['data']['title'].encode("utf-8"))
         rake.extract_keywords_from_text(post['data']['title'].encode("utf-8"))
         keywords = rake.get_ranked_phrases()
-
-
-print(summarize_page("https://www.ndtv.com/kerala-news/18-killed-due-to-landslides-and-heavy-rain-in-kerala-1897597?pfrom=home-topscroll"))
+        print(summarize_page(post['data']['url'].encode("utf-8")))
