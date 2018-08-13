31507aa59da2df46184d0696568c3f2d3ea4f689
ktech99 <kartikarora99@gmail.com>
2018-08-13 13:54:54 +0530

    create article model

--- java/redTweet/src/main/java/com/worldNews/redTweet/articleModel.java
+++ java/redTweet/src/main/java/com/worldNews/redTweet/articleModel.java
@@ -1,10 +1,59 @@
 package com.worldNews.redTweet;
 
 public class articleModel {
+    
     private String ID;
     private String URL;
     private String permaLink;
     private String title;
     private String summary;
     private String[] keywords;
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getURL() {
+        return URL;
+    }
+
+    public void setURL(String URL) {
+        this.URL = URL;
+    }
+
+    public String getPermaLink() {
+        return permaLink;
+    }
+
+    public void setPermaLink(String permaLink) {
+        this.permaLink = permaLink;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String[] getKeywords() {
+        return keywords;
+    }
+
+    public void setKeywords(String[] keywords) {
+        this.keywords = keywords;
+    }
 }
