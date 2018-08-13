2525de3d1eb1eed8995fd35a3c027c1bca0ac1bb
Kartik Arora <kartikar@uw.edu>
2018-08-13 10:13:21 +0530

    try getting image

--- python/scrape.py
+++ python/scrape.py
@@ -1,6 +1,8 @@
 import json, requests
 from rake_nltk import Rake
 from summarize import summarize_page
+import urllib2
+from bs4 import BeautifulSoup
 
 subreddit = 'worldnews'
 
@@ -11,7 +13,18 @@ r = requests.get(
 
 rake = Rake(min_length = 1, max_length = 3)
 keywords = []
+url = 'https://www.newshub.co.nz/home/world/2018/08/kiwi-tourists-urged-not-to-ride-elephants-in-thailand.html'
+# get contents from url
+content = requests.get(url).content
+# get soup
+soup = BeautifulSoup(content,'lxml') # choose lxml parser
+# find the tag : <img ... >
+image_tags = soup.findAll('img')
+# print out image urls
+for image_tag in image_tags:
+    print(image_tag.get('src'))
 # print(json.dumps(r.json()['data']['children'][0]))
+
 
 for post in r.json()['data']['children']:
     try:
@@ -23,7 +36,7 @@ for post in r.json()['data']['children']:
             # print(post['data']['title'].encode("utf-8"))
             rake.extract_keywords_from_text(post['data']['title'].encode("utf-8"))
             keywords = rake.get_ranked_phrases()
-            print(summarize_page(post['data']['url'].encode("utf-8")))
+            #print(summarize_page(post['data']['url'].encode("utf-8")))
             print "----------------------------------"
     except ValueError:
         print "parse error"
