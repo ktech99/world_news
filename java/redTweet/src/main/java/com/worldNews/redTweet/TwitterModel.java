ae29ba158dfc48dccdffafd079c7845a4dc68189
ktech99 <kartikarora99@gmail.com>
2018-08-14 11:40:34 +0530

    add pojo for twitter

--- java/redTweet/src/main/java/com/worldNews/redTweet/TwitterModel.java
+++ java/redTweet/src/main/java/com/worldNews/redTweet/TwitterModel.java
@@ -3,4 +3,29 @@ package com.worldNews.redTweet;
 public class TwitterModel {
     String user;
     String tweet;
+    String time;
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getTweet() {
+        return tweet;
+    }
+
+    public void setTweet(String tweet) {
+        this.tweet = tweet;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
 }
